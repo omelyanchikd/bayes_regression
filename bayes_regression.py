fd432d64d1718b8b481efdb74376fdde68c252c2
omelyanchikd <omelyanchikd@users.noreply.github.com>
2016-08-31 23:14:07 +0200

    Testing various options

--- bayes_regression.py
+++ bayes_regression.py
@@ -1,6 +1,7 @@
 import pandas
 
-datafile = "C:\Diana\Poland\Poland\poland_aggregate.csv"
+#datafile = "C:\Diana\Poland\Poland\poland_aggregate.csv"
+datafile = "D:\Poland\poland_aggregate.csv"
 
 poland_data = pandas.read_csv(datafile, sep = ";", decimal = ",")
 
@@ -9,15 +10,17 @@ print(poland_data)
 
 from sklearn import linear_model
 
-clf = linear_model.BayesianRidge(compute_score= True )
+clf = linear_model.BayesianRidge(compute_score= True, normalize= True)
 clf.fit(poland_data[['wages', 'workers', 'budget', 'investment']], poland_data['sold'])
 
-ols = linear_model.LinearRegression()
+ols = linear_model.LinearRegression(normalize= True)
 ols.fit(poland_data[['wages', 'workers', 'budget', 'investment']], poland_data['sold'])
 
 
-print(clf.predict([1000, 0, 1000, 0]))
-print(ols.predict([1000, 0, 1000, 0]))
+print(clf.predict([6000, 10, 10, 100]))
+print(ols.predict([6000, 10, 10, 100]))
 
 print(clf.score(poland_data[['wages', 'workers', 'budget', 'investment']], poland_data['sold']))
 print(ols.score(poland_data[['wages', 'workers', 'budget', 'investment']], poland_data['sold']))
+
+
